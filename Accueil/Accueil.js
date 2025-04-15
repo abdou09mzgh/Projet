c984fe34c85ab3703db412a0f6c23eeac342df2c
abdou <abdou09mzgh@gmail.com>
2025-04-15 23:43:54 +0100

    Premier commit

--- Accueil/Accueil.js
+++ Accueil/Accueil.js
@@ -1,4 +1,4 @@
-
+// Slide of rooms
 let current = 0;
 const slides = document.querySelectorAll('.chambre');
 
@@ -18,7 +18,50 @@ function prevSlide() {
   showSlide(current);
 }
 
-// Initialisation
+window.onload = function () {
+  const links = [
+    { buttonId: 'Apropos', sectionId: 'apropos' },
+    { buttonId: 'Chambres', sectionId: 'chambres' },
+    { buttonId: 'Restaurant', sectionId: 'restaurant' },
+    { buttonId: 'Services', sectionId: 'services' },
+    { buttonId: 'Réservation', sectionId: 'réservation' }
+  ];
+
+  links.forEach(link => {
+    const button = document.getElementById(link.buttonId);
+    const section = document.getElementById(link.sectionId);
+
+    if (button && section) {
+      button.onclick = () => {
+        section.scrollIntoView({ behavior: 'smooth' });
+      };
+    }
+  });
+
+  const btn1 = document.querySelector('.Button1');
+  const fin_btn1 = document.getElementById('réservation');
+
+
+    btn1.onclick = function () {
+      fin_btn1.scrollIntoView({
+        behavior: 'smooth'
+      });
+    };
+  
+
+  const menuItem = document.querySelector('.Button3');
+  const targetSection = document.getElementById('réservation');
+
+  if (menuItem && targetSection) {
+    menuItem.onclick = function () {
+      targetSection.scrollIntoView({
+        behavior: 'smooth'
+      });
+    };
+  }
+};
+
+
 
 
 
