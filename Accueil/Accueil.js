8d205675422d7a2111dbfe772a8c90e8f3964160
abdou <abdou09mzgh@gmail.com>
2025-04-15 17:24:29 +0100

    Premier commit

---
+++ Accueil/Accueil.js
@@ -0,0 +1,24 @@
+
+let current = 0;
+const slides = document.querySelectorAll('.chambre');
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.style.display = i === index ? 'block' : 'none';
+  });
+}
+
+function nextSlide() {
+  current = (current + 1) % slides.length;
+  showSlide(current);
+}
+
+function prevSlide() {
+  current = (current - 1 + slides.length) % slides.length;
+  showSlide(current);
+}
+
+// Initialisation
+
+
+
